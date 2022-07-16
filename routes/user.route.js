9ac9b24bcccffbdba2fa8e57106d737f5abde8d9
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-16 21:15:58 +0530

    get users and user by id

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,50 @@
+const User = require('../models/user.model')
 const router = require('express').Router();
-const { checkToken } = require('../auth/token_validation');
+const mongoose = require('mongoose');
 
+//get all users
+router.get('/arcuser', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({
+            success: 1,
+            message: 'Users fetched successfully',
+            result: users
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: 0,
+            message: 'Internal server error',
+            error: error.message
+        });
+    }
+}
+);
 
-router.get('/profile', checkToken, async (req, res, next) => {
+//get user by id
+router.get('/arcuser/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            // req.flash('error', 'Invalid Id');
+            // res.redirect('/admin/users');
+            res.status(400).json({
+                success: 0,
+                message: "Invalid User Id"
+            });
+        }
+        const data = await User.findById(id);
+        res.status(200).json({
+            success: 1,
+            message: "Profile retrieved successfully",
+            result: data
+        });
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/profile', async (req, res, next) => {
     // console.log(req.user)
     const person = req.user
     res.status(200).json({
